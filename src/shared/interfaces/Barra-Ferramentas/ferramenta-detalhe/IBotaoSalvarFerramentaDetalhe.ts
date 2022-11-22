222f811fc531b0362a2aaa23dd8acb769d8b5d67
alexandrerodgomesnet <alexandrerodgomes@hotmail.com>
2022-11-22 17:36:00 -0300

    subindo interface de props para o botao salvar

---
+++ src/shared/interfaces/Barra-Ferramentas/ferramenta-detalhe/IBotaoSalvarFerramentaDetalhe.ts
@@ -0,0 +1,6 @@
+export interface IBotaoSalvarFerramentaDetalheProps {
+    children?: string | never[];
+    mostrarBotao: boolean;
+    mostrarBotaoCarregando: boolean;
+    onClick?: () => void;
+}
