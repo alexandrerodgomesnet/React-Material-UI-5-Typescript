626b76ce06f0b87ae44c2169d45cf00de4d515b1
alexandrerodgomesnet <alexandrerodgomes@hotmail.com>
2022-11-25 15:41:23 -0300

    implementando UseDebounce.

--- src/pages/cidades/ListagemCidades.tsx
+++ src/pages/cidades/ListagemCidades.tsx
@@ -1,24 +1,31 @@
 import { useEffect, useMemo } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import { FerramentaListagem } from '../../shared/components';
+import { useDebounce } from '../../shared/hooks';
 import { LayoutBase } from '../../shared/layouts';
 import { CidadesService } from '../../shared/services/api/cidades/CidadesService';
 
 export const ListagemCidades: React.FC = () => {
 
     const [searchParams, setSearchParams] = useSearchParams();
+    const { debounce } = useDebounce();
+
     const busca = useMemo(() => {
         return searchParams.get('busca') || '';
     }, [searchParams]);
 
     useEffect(() => {
-        CidadesService.Listar(1, busca)
-            .then((result) => {
-                if(result instanceof Error)
-                    console.error(result.message);
 
-                console.log(result);
-            });
+        debounce(() =>{
+
+            CidadesService.Listar(1, busca)
+                .then((result) => {
+                    if(result instanceof Error)
+                        console.error(result.message);
+
+                    console.log(result);
+                });
+        });
     }, [busca]);
 
     return (
